ff228ce6819d33868ba9ab3f08100cb69a60fe3c
Karthika M <karthikam0912@gmail.com>
2024-01-22 22:24:11 +0530

    fifth commit

--- promise.js
+++ promise.js
@@ -1,2 +1,25 @@
 // Prmoise - successfull and unsuccessful
 
+const arr=['s1','s2','s3']
+const marks=[12,14,15,11]
+const rank=[3,2,1,4]
+const student={
+    "name":"kavya",
+    "marks":12,
+    "rank":3
+}
+const student2={
+    "name":"priya",
+    "marks":100,
+    "rank":1
+}
+const student3={
+    "name":"karthika",
+    "marks":99,
+    "rank":2
+}
+const studentdetails=[student,student2,student3]
+console.log(studentdetails)
+const newPrommise=new Promise(function(resolve,reject){
+    
+})

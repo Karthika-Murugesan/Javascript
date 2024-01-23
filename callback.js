549fcec56266a3f9440a676583c67b03dcea28c7
Karthika M <karthikam0912@gmail.com>
2024-01-23 12:12:12 +0530

    3rd commit

--- callback.js
+++ callback.js
@@ -1,13 +1,25 @@
 //a fucntion is embedded inside by another funtion through parameter
 //set time out for the function-within this time,complete the task and report the result,if not,then move on to the next function to be executed
 
-function kk(name,callback){
-         setTimeout(function(){
-        console.log(name)
-        callback()
-         },2000)
+// function kk(name,callback){
+//          setTimeout(function(){
+//         console.log(name)
+//         callback()
+//          },2000)
+// }
+// function mk(){
+//     console.log("I dont need hello")
+// }
+// kk("Hello ",mk)
+
+function database(){
+   console.log("Form submitted successfully..!!")
 }
-function mk(){
-    console.log("I dont need hello")
+
+function submitForm(buttonClick,db){ // function definition
+   setTimeout(function(){
+    console.log(buttonClick)
+        db()
+   },2000)
 }
-kk("Hello ",mk)
+submitForm("Form submit button clicked",database) // Function calling
